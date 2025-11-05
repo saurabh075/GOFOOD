2e271ffb96eefcef859389bc632c590312342c66
saurabh075 <153181198+saurabh075@users.noreply.github.com>
2025-11-05 16:40:17 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,14 @@
+const sum = (a,b) => a+b;
+const mul = (a,b) => a*b;
+const g = 9.9;
+const pi = 3.14;
+
+
+let obj = {
+    sum:sum,
+    mul:mul,
+    g:g,
+    pi:pi,
+};
+
+module.exports = obj;
